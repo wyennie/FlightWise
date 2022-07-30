4b6ffe9118f9929ff88643432772d8f03f259c56
will <wryennie@gmail.com>
2022-07-30 16:19:02 -0400

    added raw data from data.js to App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,24 @@
 import React, { Component } from 'react';
+import Info from './data.js'
 import './App.css';
 
+const Routes = () => {
+  return (
+    <table>
+      <tbody>
+        {Info.routes.map(info => {
+          return (
+            <tr key={info.airline + info.src + info.dest}>
+              <td>{info.airline}</td>
+              <td>{info.src}</td>
+              <td>{info.dest}</td>
+            </tr>
+          )
+        })}
+      </tbody>
+    </table>
+  )
+}
 const App = () => (
   <div className="app">
   <header className="header">
@@ -11,6 +29,7 @@ const App = () => (
       Welcome to the app!
     </p>
   </section>
+  <Routes />
 </div>
 )
 
